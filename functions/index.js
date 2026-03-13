2ca48488d97db14f9614c54fd4ed26fdb378b039
DeepMobb <hhnh.rockwool@gmail.com>
2026-03-13 13:09:31 +0100

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -9,7 +9,7 @@ exports.sendLogMail = functions.firestore
   .onCreate(async (snap) => {
     const data = snap.data();
 
-    sgMail.setApiKey("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"); // ← indsæt din rigtige nøgle her
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
     let mailBody = "Daglige logs fra Ropex Logbog App:\n\n";
     data.logs.forEach((d, i) => {
